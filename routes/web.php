c44af57d945afdcdf3ee7b748f4ff64d121fbd5a
mehrdad <m.ebrahimi.talo1990@gmail.com>
2021-09-27 10:43:40 +0330

    frontend view complete

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/products/all', function () {
+    return view('frontend.products.all');
 });
